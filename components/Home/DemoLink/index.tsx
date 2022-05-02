d8d5c240eb3ca6f917e4a6710f66782aa5d5657e
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2022-05-02 08:22:41 -0300

    feat: demo link component text

--- components/Home/DemoLink/index.tsx
+++ components/Home/DemoLink/index.tsx
@@ -2,13 +2,11 @@ import React from 'react'
 
 export default function DemoLink() {
   return (
-    <div className="bg-gray-50">
+    <div className="">
       <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
-        <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+        <h2 className="text-3xl font-extrabold tracking-tight text-indigo-600 sm:text-4xl">
           <span className="block">Ready to dive in?</span>
-          <span className="block text-indigo-600">
-            Start your free trial today.
-          </span>
+          <span className="block text-indigo-600">Start use it now.</span>
         </h2>
         <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
           <div className="inline-flex rounded-md shadow">
@@ -24,7 +22,7 @@ export default function DemoLink() {
               href="#"
               className="inline-flex items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-indigo-600 hover:bg-indigo-50"
             >
-              Learn more
+              Live Demo
             </a>
           </div>
         </div>
